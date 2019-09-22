33a45b028177d3ec18c625e5ca28a4b4501dbf5a
tianshisheng <tianshisheng@DESKTOP-5PCC2DM>
2019-09-22 12:18:43 +0800

    工作票，操作票添加2小時逻辑

--- WebContent/pagecodes/PROCEDURE/GET_CZP_COUNT.sql
+++ WebContent/pagecodes/PROCEDURE/GET_CZP_COUNT.sql
@@ -3,10 +3,10 @@ DROP PROCEDURE GET_CZP_COUNT;
 create procedure GET_CZP_COUNT()
 begin
 
-select DATE_FORMAT(S_KPSJ,'%Y-%m-%d') kpsj,S_CZBZ czbz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, count(S_CZBZ) count
+select DATE_FORMAT(DATE_SUB(S_KPSJ,INTERVAL 2 HOUR),'%Y-%m-%d') kpsj,S_CZBZ czbz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, count(S_CZBZ) count
 from t_czpsc,T_CZPF
 where S_CZBZ in ('01', '02', 'hd03', '21') and S_DZZT<>'CZPZT040' and S_ZZ='001017' and T_CZPF.S_FID=T_CZPSC.S_ID
-group by DATE_FORMAT(S_KPSJ,'%Y-%m-%d'),S_CZBZ, S_BPDD, if(S_BPDD='01_JK', S_JZ,'');
+group by DATE_FORMAT(DATE_SUB(S_KPSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_CZBZ, S_BPDD, if(S_BPDD='01_JK', S_JZ,'');
 
 end
 
--- WebContent/pagecodes/PROCEDURE/GET_GZP_XKXKR_COUNT.sql
+++ WebContent/pagecodes/PROCEDURE/GET_GZP_XKXKR_COUNT.sql
@@ -3,58 +3,58 @@ DROP PROCEDURE GET_GZP_XKXKR_COUNT;
 create procedure GET_GZP_XKXKR_COUNT()
 begin
 
-(select 'T_DQYZGZP','电气一种工作票',DATE_FORMAT(S_XKKSGZSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
+(select 'T_DQYZGZP','电气一种工作票',DATE_FORMAT(DATE_SUB(S_XKKSGZSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
 from (select * from T_DQYZGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_DQYZGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XKKSGZSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from s_dy_f1 where S_A<>'' GROUP BY s_fid) f1 on f1.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount2 from t_dy_f2 where S_A<>'' or S_B<>'' GROUP BY s_fid) f2 on f2.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XKKSGZSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
+group by DATE_FORMAT(DATE_SUB(S_XKKSGZSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
 
 UNION All
 
-(select 'T_DQEZGZP','电气二种工作票',DATE_FORMAT(S_XK_XKRQZSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
+(select 'T_DQEZGZP','电气二种工作票',DATE_FORMAT(DATE_SUB(S_XK_XKRQZSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
 from (select * from T_DQEZGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_DQEZGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_XKRQZSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from s_d2_aqcs where S_BXCQDAQCS<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount2 from s_d2_bcaqcs where S_BCDAC<>'' GROUP BY s_fid) bcaqcs on bcaqcs.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_XKRQZSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
+group by DATE_FORMAT(DATE_SUB(S_XK_XKRQZSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
 
 UNION All
 
-(select 'T_RLYZGZP','热力机械工作票',DATE_FORMAT(S_XK_XKRSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0))+sum(IFNULL(aqcsCount3,0)) count
+(select 'T_RLYZGZP','热力机械工作票',DATE_FORMAT(DATE_SUB(S_XK_XKRSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0))+sum(IFNULL(aqcsCount3,0)) count
 from (select * from T_RLYZGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_RLYZGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_XKRSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from t_rly_aqcs where S_BXCQDAC<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount2 from t_rly_bcaqcs where S_BCDAC<>'' GROUP BY s_fid) bcaqcs on bcaqcs.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount3 from t_rly_yjcaqcs where S_YJCDBH<>'' GROUP BY s_fid) yjcaqcs on yjcaqcs.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_XKRSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
+group by DATE_FORMAT(DATE_SUB(S_XK_XKRSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
 
 UNION All
 
-(select 'T_YJDHGZP','一级动火工作票',DATE_FORMAT(S_XK_YXXKDHSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0)) count
+(select 'T_YJDHGZP','一级动火工作票',DATE_FORMAT(DATE_SUB(S_XK_YXXKDHSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0)) count
 from (select * from T_YJDHGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_YJDHGZP.S_WXZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_YXXKDHSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from t_dhy_aqcs where S_DHBMYCQDAC<>'' or S_YXYCQDAC<>'' or S_XFYCQDAC<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_YXXKDHSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
+group by DATE_FORMAT(DATE_SUB(S_XK_YXXKDHSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
 
 UNION All
 
-(select 'T_EJDHGZP','二级动火工作票',DATE_FORMAT(S_XK_YXXKDHSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0)) count
+(select 'T_EJDHGZP','二级动火工作票',DATE_FORMAT(DATE_SUB(S_XK_YXXKDHSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0)) count
 from (select * from T_EJDHGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_EJDHGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_YXXKDHSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from t_dhe_aqcs where S_DHBMYCQDAC<>'' or S_YXYCQDAC<>'' or S_XFYCQDQC<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_YXXKDHSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
+group by DATE_FORMAT(DATE_SUB(S_XK_YXXKDHSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''))
 
 UNION All
 
-(select 'T_RKGZP','热控工作票',DATE_FORMAT(S_XK_XKRSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDJ bpdd, if(S_BPDJ='01_JK', S_ZJ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
+(select 'T_RKGZP','热控工作票',DATE_FORMAT(DATE_SUB(S_XK_XKRSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDJ bpdd, if(S_BPDJ='01_JK', S_ZJ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
 from (select * from T_RKGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_RKGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_XKRSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from t_rk_aqcs where S_YXRYZXD<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount2 from t_rk_aqcs2 where S_BCDAQCS<>'' GROUP BY s_fid) aqcs2 on aqcs2.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_XKRSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDJ, if(S_BPDJ='01_JK', S_ZJ,''))
+group by DATE_FORMAT(DATE_SUB(S_XK_XKRSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDJ, if(S_BPDJ='01_JK', S_ZJ,''))
 
 UNION All
 
-(select 'T_YJQXGZP','应急抢修工作票',DATE_FORMAT(S_XK_GZXKRSPSJ,'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
+(select 'T_YJQXGZP','应急抢修工作票',DATE_FORMAT(DATE_SUB(S_XK_GZXKRSPSJ,INTERVAL 2 HOUR),'%Y-%m-%d') xksj,S_XK_BZBM bz, S_BPDD bpdd, if(S_BPDD='01_JK', S_JZ,'') jz, sum(IFNULL(aqcsCount,0))+sum(IFNULL(aqcsCount2,0)) count
 from (select * from T_YJQXGZP where S_DEL<>'1' and S_GZPZT<>'GZPZT051' and T_YJQXGZP.S_ZZ='001017' and S_XK_BZBM in ('01', '02', 'hd03', '21') and S_XK_GZXKRSPSJ<>'') gzp
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount from t_yj_aqcs where S_BXCQDAC<>'' GROUP BY s_fid) aqcs on aqcs.s_fid=gzp.S_ID
 LEFT OUTER JOIN (select s_fid,count(*) aqcsCount2 from t_yj_bcaqcs where S_ZBRYBCDAC<>'' GROUP BY s_fid) bcaqcs on bcaqcs.s_fid=gzp.S_ID
-group by DATE_FORMAT(S_XK_GZXKRSPSJ,'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''));
+group by DATE_FORMAT(DATE_SUB(S_XK_GZXKRSPSJ,INTERVAL 2 HOUR),'%Y-%m-%d'),S_XK_BZBM, S_BPDD, if(S_BPDD='01_JK', S_JZ,''));
 
 end
 
